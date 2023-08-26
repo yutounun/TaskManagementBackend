807859e123f5d9c8f857f5ccde20a8d794302e7f
yutounun <geek.yuto@gmail.com>
2023-08-26 12:02:49 +0900

    :fix: Remove unused codes and add error func

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from routes.api import router as api_router
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import sessionmaker
 from starlette.requests import Request
 from db import engine
 
@@ -23,6 +23,7 @@ app.add_middleware(
 
 app.include_router(api_router)
 
+# Start the server when the code is executed
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8001, log_level="info", reload=True)
     print("running")
@@ -30,8 +31,11 @@ if __name__ == "__main__":
 
 @app.middleware("http")
 async def db_session_middleware(request: Request, call_next):
+    # each router will have request with state.db property
     request.state.db = SessionLocal()
+    # proceed to the next middleware or the route handler
     response = await call_next(request)
+    # route handler may return a response
     request.state.db.close()
     return response
 
