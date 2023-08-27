f20432ccf632d60eb10d06ee3ddd66edc12cc6f3
yutounun <geek.yuto@gmail.com>
2023-08-27 20:06:14 +0900

    complete implimenting test for task api

--- src/endpoints/test_task.py
+++ src/endpoints/test_task.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from starlette import status
 from main import app
+import pytest
 
 client = TestClient(app)
 
 
-request_body = {
+LOGIN_DATA = {"username": "master", "password": "password"}
+TASK_DATA = {
     "title": "test",
     "status": "test",
     "man_hour_min": 1,
@@ -16,62 +18,70 @@ request_body = {
     "project_key": "test",
     "user_key": "test",
 }
+EDIT_TASK_DATA = {
+    "title": "update test",
+    "status": "update test",
+    "man_hour_min": 2,
+    "to_date": "2024-08-14T15:32:00Z",
+    "from_date": "2024-08-14T15:32:00Z",
+    "priority": 2,
+    "project_key": "update test",
+    "user_key": "update test",
+}
 
 
-def login():
-    login_response = client.post(
-        "/auth/login/",
-        data={"username": "yuto", "password": "password"},
-    )
+@pytest.fixture
+def headers():
+    login_response = client.post("/auth/login/", data=LOGIN_DATA)
     assert login_response.status_code == status.HTTP_200_OK
     token = login_response.json()["access_token"]
     return {"Authorization": f"Bearer {token}"}
 
 
-def create_task():
-    headers = login()
-    create_response = client.post("/tasks/", headers=headers, json=request_body)
-    assert create_response.status_code == status.HTTP_200_OK
-    return create_response
+@pytest.fixture
+def created_task(headers):
+    response = client.post("/tasks/", headers=headers, json=TASK_DATA)
+    assert response.status_code == status.HTTP_200_OK
+    yield response.json()  # return response
+
+    # delete create data after each test
+    task_id = response.json()["id"]
+    del_response = client.delete("/tasks/" + task_id, headers=headers)
+    assert del_response.status_code == status.HTTP_200_OK
 
 
-def test_create_and_read():
-    headers = login()
-    create_response = create_task()
+def extract_task_data(task):
+    extract_data = {key: task[key] for key in TASK_DATA}
+    extract_data["to_date"] = extract_data["to_date"] + "Z"
+    extract_data["from_date"] = extract_data["from_date"] + "Z"
+    return extract_data
 
-    task_id = create_response.json()["id"]
+
+def test_create_and_read(headers, created_task):
+    task_id = created_task["id"]
     response = client.get("/tasks/" + task_id, headers=headers)
     assert response.status_code == status.HTTP_200_OK
-    obj_res = response.json()
-    obj_res = {
-        "title": obj_res["title"],
-        "status": obj_res["status"],
-        "man_hour_min": obj_res["man_hour_min"],
-        "to_date": obj_res["to_date"] + "Z",
-        "from_date": obj_res["from_date"] + "Z",
-        "priority": obj_res["priority"],
-        "project_key": obj_res["project_key"],
-        "user_key": obj_res["user_key"],
-    }
-    assert obj_res == request_body
 
-
-def test_create_and_remove():
-    headers = login()
-    create_response = create_task()
-
-    task_id = create_response.json()["id"]
-    response = client.get("/tasks/" + task_id, headers=headers)
+    obj_res = extract_task_data(response.json())
     assert response.status_code == status.HTTP_200_OK
-    obj_res = response.json()
-    obj_res = {
-        "title": obj_res["title"],
-        "status": obj_res["status"],
-        "man_hour_min": obj_res["man_hour_min"],
-        "to_date": obj_res["to_date"] + "Z",
-        "from_date": obj_res["from_date"] + "Z",
-        "priority": obj_res["priority"],
-        "project_key": obj_res["project_key"],
-        "user_key": obj_res["user_key"],
-    }
-    assert obj_res == request_body
+    assert obj_res == TASK_DATA
+
+    response = client.get("/tasks/", headers=headers)
+    assert response.status_code == status.HTTP_200_OK
+    print("response.json()", response.json())
+
+
+def test_edit_and_read(headers, created_task):
+    task_id = created_task["id"]
+    response = client.put("/tasks/" + task_id, headers=headers, json=EDIT_TASK_DATA)
+    assert response.status_code == status.HTTP_200_OK
+
+    obj_res = extract_task_data(response.json())
+    assert obj_res == EDIT_TASK_DATA
+
+
+def test_create_and_remove(headers, created_task):
+    task_id = created_task["id"]
+    response = client.delete("/tasks/" + task_id, headers=headers)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"status": "OK"}
